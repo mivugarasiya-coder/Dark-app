5266b8c30bf01be006c9b4e1491f27ab9f2a6c41
mivugarasiya-coder <mivugarasiya@gmail.com>
2026-09-10 12:33:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-import google.generativeai as genai
+from google import genai
 import streamlit as st
 
-genai.configure(api_key="AQ.Ab8RN6Kw9r70bRlfZYniekYVrlw-LyfKRroPgVut8vLvZdiFNw")
+client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
 
 st.title("Evil GPT")
-model = genai.GenerativeModel("gemini-1.5-flash")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -19,6 +18,9 @@ if prompt := st.chat_input("Ask anything..."):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        response = model.generate_content(prompt)
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt,
+        )
         st.markdown(response.text)
         st.session_state.messages.append({"role": "assistant", "content": response.text})
